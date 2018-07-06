3fb35c2f6b899ebb0bdaac6136e74c8368f74b8c
TJH <thomasjohnhall@gmail.com>
2018-07-06 20:02:26 +0100

    fetch blob sound

--- components/Player.js
+++ components/Player.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
+import RNFetchBlob from 'rn-fetch-blob'
 
 var Sound = require('react-native-sound');
 Sound.setCategory('Playback');
@@ -14,21 +15,52 @@ export class Player extends Component {
   }
 
   playRooney() {
-    const Rooney = new Sound(
-        this.props.url,
-
-       null, (error) => {
-      if (error) {
-        console.log('failed to load the sound', error);
-        return;
-        }
-      // loaded successfully
-      Rooney.play(() => {
-        Rooney.release();
-      });
+    function doSound(path) {
+      // These timeouts are a hacky workaround for some issues with react-native-sound.
+      // See https://github.com/zmxv/react-native-sound/issues/89.
+      setTimeout(() => {
+        const Rooney = new Sound(path, null, (error) => {
+          if (error) {
+            console.log('failed to load the sound', error);
+            return;
+          }
+        })
+          // loaded successfully
+          setTimeout(() => {
+          Rooney.play((success) => {
+            if (success) {
+              console.log('successfully finished playing')
+            } else {
+              console.log('playback failed due to audio decoding errors')
+            }
+          })
+        }, 100)
+      }, 100)
       console.log('duration in seconds: ' + Rooney.getDuration());
-    })
-  }
+      Rooney.release();
+    }
+
+    var pat = /^https?:\/\//i;
+    if (pat.test(this.props.url)) {
+    RNFetchBlob
+      .config({
+        path: RNFetchBlob.fs.dirs.CacheDir + '/voiceMsgReceived.aac',
+        fileCache: true,
+        appendExt: 'aac',
+      })
+      .fetch('GET', this.props.url, {
+        // your custom headers here
+        'x-amz-security-token': this.props.token
+      })
+      .then((res) => {
+        console.log('The file saved to ', res.path())
+      })
+      .then((path) => {
+        doSound(path);
+      })
+      }
+
+    }
 
   render() {
     return (
